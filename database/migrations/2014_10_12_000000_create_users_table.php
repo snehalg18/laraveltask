9fc78d27ec77423675617c715adef8f5814c4593
snehalg18 <snehalgujar18@gmail.com>
2025-02-02 23:46:23 +0530

    Added role enum to users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,12 +16,12 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('address');
-            $table->string('role');
-            $table->string('google_id');
+            $table->string('password')>nullable();
+            $table->string('address')>nullable();
+            $table->enum('role', ['Admin', 'Employee'])->default('Employee');
+            $table->string('google_id')>nullable();
             $table->string('dob');
-            $table->rememberToken();
+            $table->rememberToken()>nullable();
             $table->timestamps();
         });
 
